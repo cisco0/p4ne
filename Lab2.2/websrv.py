987cb86f67f3cb5150dc80e8834c6887a171a493
cisco0 <va.martynov@jet.msk.su>
2017-08-29 18:43:00 +0300

    Start

--- Lab2.2/websrv.py
+++ Lab2.2/websrv.py
@@ -21,7 +21,7 @@ def cl(s):
         return ("INT", s.groups())
     else:
         return ("UNCLASSIFIED", )
-
+current_host = ""
 x = glob.glob("C:\\Users\\va.martynov\\Seafile\\p4ne_training\\config_files\\*.txt")
 for i in x:
     with open(i) as f:
@@ -31,10 +31,10 @@ for i in x:
 
             a = cl(m)
             if a[0] == "HOST":
-                hosts[a[1]] = {"name": a[1], "ip": []}
-                ip[a[1]] = {"name": a[1], "ip": []}
-            if a[0] == "IP":
-                    hosts[a[1]] = {"ip": a[1], "ip": []}
+                hosts[a[1]] = {"name": a[1]}
+                current_host = a[1]
+            if a[0] == "IP" and current_host:
+                    hosts[current_host]["ip"] = a[1]
 
 
 app = Flask(__name__)
@@ -46,11 +46,20 @@ def index():
 def page():
  return jsonify(str(sys.__dict__))
 @app.route('/config')
+@app.route('/config/')
 def config():
   return jsonify(list(ip.keys()))
 @app.route('/config/ip')
 def configip():
   return jsonify(list(hosts.keys()))
+@app.route('/config/<hostname>')
+def ip_info(hostname):
+    for h in hosts.keys():
+        if hosts[h]['name'][0] == hostname:
+            return jsonify(hosts[h]['ip'])
+    return jsonify('Not found')
+
 
 if __name__ == "__main__":
+    print(hosts)
     app.run(debug=True)
